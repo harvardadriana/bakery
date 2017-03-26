9a12f145a394969d896f38f9b43842596299813b
harvardadriana <adrianarossettisugih@g.harvard.edu>
2017-03-27 02:12:30 +0900

    Added debugbar, rych random, and log viewer

--- config/app.php
+++ config/app.php
@@ -163,6 +163,9 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Barryvdh\Debugbar\ServiceProvider::class,
+        Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
@@ -225,6 +228,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,11 @@
 
 
 
-
-
-
-
 /**
  * Practice - log viewer
  */
 if(config('app.env') == 'local') {
-	#Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+	Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 }
 
 
